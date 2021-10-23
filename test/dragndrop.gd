8ecbf0f75d672063085955459f79d45bafbdf8eb
SkyGovernment <headsinkart@protonmail.com>
2021-10-23 17:58:35 +0800

    Add Mouse not pressed

--- test/dragndrop.gd
+++ test/dragndrop.gd
@@ -12,3 +12,8 @@ func _physics_process(delta):
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25*delta)
 		
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and not event.pressed:
+			selected = false
+			
