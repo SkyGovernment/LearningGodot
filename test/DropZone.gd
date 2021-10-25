170923e77f664210d644920c1b8bb26fb4fec1e9
SkyGovernment <headsinkart@protonmail.com>
2021-10-25 23:27:29 +0800

    Dropzone

--- test/DropZone.gd
+++ test/DropZone.gd
@@ -2,3 +2,11 @@ extends Position2D
 
 func _draw():
 	draw_circle(Vector2.ZERO, 75, Color.rebeccapurple)
+
+func select():
+	for child in get_tree().get_nodes_in_group("Zone"):
+		child.deselect()
+	modulate = Color.webmaroon
+	
+func deselect():
+	modulate = Color.antiquewhite
