45a2f58914eb0b17910b7cfae09cf8dc2112dfe3
SkyGovernment <headsinkart@protonmail.com>
2021-10-27 15:26:29 +0800

    Object to Dropzone

--- test/dragndrop.gd
+++ test/dragndrop.gd
@@ -16,12 +16,19 @@ func _on_Area2D_input_event(viewport, event, shape_idx):
 func _physics_process(delta):
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25*delta)
-	# else:
-	#	global_position = lerp(global_position, rest_point, 10 * delta) # Sprite to rest_point
+	else:
+		global_position = lerp(global_position, rest_point, 10 * delta) # Sprite to rest_point
 		
 	
 func _input(event):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton: # Check Mouse Input
 		if event.button_index == BUTTON_LEFT and not event.pressed:
 			selected = false
+			var shortest_dist = 75
+			for child in rest_nodes:
+				var distance = global_position.distance_to(child.global_position)
+				if distance < shortest_dist:
+					child.select()
+					rest_point = child.global_position
+					shortest_dist = distance
 			
